6a7711baf121d2fea868fb4907d20094d536bbed
Prado <mahery.rsh@gmail.com>
2021-12-18 12:48:39 +0100

    Add selector module for DOM manipulation
    jQuery like

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,23 @@
+import { get, getAll, getById, clearEvents } from './libs/Selector/selector.js';
 
 const main = () => {
+    getAll('.btn-action').click(function (e) {
+        console.log(this.textContent);
+    });
+    console.log(getAll('.btn-action').get());
+    console.log(getById('btn-test2').text());
+    console.log(getById('btn-test2').html());
+    console.log(get('.btn-action').hasClass('shut'));
+    console.log(get('.btn-action').removeClass('shut'));
+    console.log(get('#btn-test3').addClass('HEYY'));
+    console.log(get('h1.text-center').hide());
+    console.log(get('h1.text-center').show());
+    console.log(getById('btn-test2').text());
+    console.log(getById('btn-test1').css('color', 'red'));
 
+    setTimeout(() => {
+        clearEvents();
+    }, 5000);
 };
 
 main();
