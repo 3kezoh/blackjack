74cb18120ee1dec50785499b3751cb5f69727a28
3kezoh <3kezoh@gmail.com>
2021-12-30 16:35:23 +0100

    📝 Add Deck JSDoc

--- src/libs/Deck/index.js
+++ src/libs/Deck/index.js
@@ -2,16 +2,29 @@ import DeckService from "./service.js";
 
 export default function Deck() {
   this.deckService = new DeckService();
+  /**
+   * @type {string}
+   */
   this.deck_id = null;
+
+  /**
+   * @type {number}
+   */
   this.remaining = 0;
 }
 
+/**
+ * Shuffles the deck.
+ */
 Deck.prototype.shuffle = async function () {
   const { deck_id, remaining } = await this.deckService.shuffle();
   this.deck_id = deck_id;
   this.remaining = remaining;
 };
 
+/**
+ * Reshuffles the deck with new cards.
+ */
 Deck.prototype.reshuffle = async function () {
   if (this.deck_id) {
     const { remaining } = await this.deckService.shuffle(this.deck_id);
@@ -19,6 +32,10 @@ Deck.prototype.reshuffle = async function () {
   }
 };
 
+/**
+ * Draws a card from the deck.
+ * @returns {Promise<Card>}
+ */
 Deck.prototype.draw = async function () {
   const { cards, remaining } = await this.deckService.draw(this.deck_id, 1);
   this.remaining = remaining;
