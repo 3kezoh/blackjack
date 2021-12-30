ed98e83827981161d9c3393fbd0907ba92334763
3kezoh <3kezoh@gmail.com>
2021-12-30 16:34:14 +0100

    📝 Add Card JSDoc

--- src/libs/Card/index.js
+++ src/libs/Card/index.js
@@ -1,11 +1,40 @@
-export default function Card(value, code, image, suit) {
+/**
+ * Represents a card.
+ * @param {Object} card
+ * @param {string} card.value
+ * @param {string} card.code
+ * @param {string} card.image
+ * @param {string} card.suit
+ */
+export default function Card({ value, code, image, suit }) {
+  /**
+   * @type {string}
+   */
   this.code = code;
+
+  /**
+   * @type {string}
+   */
   this.image = image;
+
+  /**
+   * @type {string}
+   */
   this.value = value;
+
+  /**
+   * @type {string}
+   */
   this.suit = suit;
+
+  /**
+   * @type {boolean}
+   */
   this.isFlipped = false;
 }
-
+/**
+ * Flip a card.
+ */
 Card.prototype.flip = function () {
   this.isFlipped = !this.isFlipped;
 };
