ada34065d6d397ddfafb90518f823d6089f9da49
3kezoh <3kezoh@gmail.com>
2022-01-09 16:34:42 +0100

    🎨 Format with prettier

--- src/libs/Deck/index.js
+++ src/libs/Deck/index.js
@@ -1,49 +1,49 @@
-import DeckService from "./service.js";
 import Card from "../Card/index.js";
+import DeckService from "./service.js";
 
 export default function Deck() {
-  this.deckService = new DeckService();
-  /**
-   * @type {string}
-   */
-  this.deck_id = null;
+    this.deckService = new DeckService();
+    /**
+     * @type {string}
+     */
+    this.deck_id = null;
 
-  /**
-   * @type {number}
-   */
-  this.remaining = 0;
+    /**
+     * @type {number}
+     */
+    this.remaining = 0;
 }
 
 /**
  * Creates a Deck instance from a regular object
  * @returns {Deck}
  */
-Deck.create = data => {
-  const instance = new Deck();
+Deck.create = (data) => {
+    const instance = new Deck();
 
-  instance.deck_id = data.deck_id;
-  instance.remaining = data.remaining;
+    instance.deck_id = data.deck_id;
+    instance.remaining = data.remaining;
 
-  return instance;
+    return instance;
 };
 
 /**
  * Shuffles the deck.
  */
 Deck.prototype.shuffle = async function () {
-  const { deck_id, remaining } = await this.deckService.shuffle();
-  this.deck_id = deck_id;
-  this.remaining = remaining;
+    const { deck_id, remaining } = await this.deckService.shuffle();
+    this.deck_id = deck_id;
+    this.remaining = remaining;
 };
 
 /**
  * Reshuffles the deck with new cards.
  */
 Deck.prototype.reshuffle = async function () {
-  if (this.deck_id) {
-    const { remaining } = await this.deckService.shuffle(this.deck_id);
-    this.remaining = remaining;
-  }
+    if (this.deck_id) {
+        const { remaining } = await this.deckService.shuffle(this.deck_id);
+        this.remaining = remaining;
+    }
 };
 
 /**
@@ -51,14 +51,14 @@ Deck.prototype.reshuffle = async function () {
  * @returns {Promise<Card>}
  */
 Deck.prototype.draw = async function () {
-  const { cards, remaining } = await this.deckService.draw(this.deck_id, 1);
-  const [card] = cards;
-  this.remaining = remaining;
-  return new Card(card);
+    const { cards, remaining } = await this.deckService.draw(this.deck_id, 1);
+    const [card] = cards;
+    this.remaining = remaining;
+    return new Card(card);
 };
 
 Deck.prototype[Symbol.asyncIterator] = async function* () {
-  for (let i = this.remaining; i > 0; i--) {
-    yield this.draw();
-  }
+    for (let i = this.remaining; i > 0; i--) {
+        yield this.draw();
+    }
 };
--- src/scripts/libs/Game/game.js
+++ src/scripts/libs/Game/game.js
@@ -1,8 +1,8 @@
-import Constants from '../../constants.js';
-import Player from '../../../libs/Player/index.js';
-import Deck from '../../../libs/Deck/index.js';
-import Displayer from '../Displayer/displayer.js';
-import { get, getById, getAll, clearSelectorEvents } from '../Selector/selector.js';
+import Deck from "../../../libs/Deck/index.js";
+import Player from "../../../libs/Player/index.js";
+import Constants from "../../constants.js";
+import Displayer from "../Displayer/displayer.js";
+import { clearSelectorEvents, get, getById } from "../Selector/selector.js";
 
 const Game = function () {
     this.deck = new Deck();
@@ -10,7 +10,7 @@ const Game = function () {
     this.status = Constants.GAME_STATUS_READY;
 };
 
-Game.create = data => {
+Game.create = (data) => {
     const instance = new Game();
 
     instance.deck = Deck.create(data.deck);
@@ -32,11 +32,11 @@ Game.prototype.init = function () {
     }
 
     /* Event listeners */
-    getById('#action-deck').click(() => this.start());
-    getById('#action-stop').click(() => this.stop());
-    getById('#action-restart').click(() => this.restart());
-    getById('#action-stand').click(() => this.stand());
-    getById('#action-hit').click(() => this.draw());
+    getById("#action-deck").click(() => this.start());
+    getById("#action-stop").click(() => this.stop());
+    getById("#action-restart").click(() => this.restart());
+    getById("#action-stand").click(() => this.stand());
+    getById("#action-hit").click(() => this.draw());
 };
 
 Game.prototype.isRunning = function () {
@@ -47,59 +47,59 @@ Game.prototype.start = function () {
     if (this.status !== Constants.GAME_STATUS_READY) {
         return false;
     }
-    console.log('start');
+    console.log("start");
 
     this.status = Constants.GAME_STATUS_RUNNING;
-    getById('#action-deck').click(() => this.draw());
-    getById('#action-stand').attr('disabled', true);
-    getById('#action-stop').removeClass('hidden');
-    get('.deck-container').removeClass('initial-center');
-    get('.bj-scoreboard').removeClass('hidden');
-    get('.bj-actions').removeClass('hidden');
+    getById("#action-deck").click(() => this.draw());
+    getById("#action-stand").attr("disabled", true);
+    getById("#action-stop").removeClass("hidden");
+    get(".deck-container").removeClass("initial-center");
+    get(".bj-scoreboard").removeClass("hidden");
+    get(".bj-actions").removeClass("hidden");
 };
 
 Game.prototype.stop = function () {
     if (this.status === Constants.GAME_STATUS_READY) {
         return false;
     }
-    console.log('stop');
+    console.log("stop");
     this.status = Constants.GAME_STATUS_READY;
-    getById('#player-hand').html('');
-    getById('#action-deck').click(() => this.start());
-    getById('#action-stop').addClass('hidden');
-    getById('#action-restart').addClass('hidden');
-    get('.deck-container').addClass('initial-center');
-    get('.bj-scoreboard').addClass('hidden');
-    get('.bj-actions').addClass('hidden');
+    getById("#player-hand").html("");
+    getById("#action-deck").click(() => this.start());
+    getById("#action-stop").addClass("hidden");
+    getById("#action-restart").addClass("hidden");
+    get(".deck-container").addClass("initial-center");
+    get(".bj-scoreboard").addClass("hidden");
+    get(".bj-actions").addClass("hidden");
 };
 
 Game.prototype.restart = function () {
-    let hasCards = getById('#player-hand').html() !== ''; // TODO: change this condition with some Player.hasCard function
+    let hasCards = getById("#player-hand").html() !== ""; // TODO: change this condition with some Player.hasCard function
     if (!this.isRunning() || !hasCards) {
         return false;
     }
-    console.log('restart');
+    console.log("restart");
     this.stop();
     this.start();
 };
 
 Game.prototype.stand = function () {
-    let hasCards = getById('#player-hand').html() !== ''; // TODO: change this condition with some Player.hasCard function
+    let hasCards = getById("#player-hand").html() !== ""; // TODO: change this condition with some Player.hasCard function
     if (!this.isRunning() || !hasCards) {
         return false;
     }
-    console.log('stand');
+    console.log("stand");
     this.status = Constants.GAME_STATUS_FINISHED;
-    get('.bj-actions').addClass('hidden');
+    get(".bj-actions").addClass("hidden");
 };
 
 Game.prototype.draw = function () {
     if (!this.isRunning()) {
         return false;
     }
-    console.log('draw');
-    getById('#action-restart').removeClass('hidden');
-    getById('#action-stand').removeAttr('disabled');
+    console.log("draw");
+    getById("#action-restart").removeClass("hidden");
+    getById("#action-stand").removeAttr("disabled");
 
     Displayer.displayPlayerCard(null);
 };
@@ -108,7 +108,7 @@ Game.prototype.cancelDraw = function () {
     if (!this.isRunning()) {
         return false;
     }
-    console.log('cancel draw');
+    console.log("cancel draw");
 };
 
 Game.prototype.clear = function () {
@@ -126,6 +126,6 @@ const clearAllInterval = () => {
     for (const interval of intervalStorage) {
         clearInterval(interval);
     }
-}
+};
 
 export default Game;
